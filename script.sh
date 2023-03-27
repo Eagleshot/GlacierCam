a453b96197b54dd5565500c56839196d147c096a
Noel <75380532+Eagleshot@users.noreply.github.com>
2023-03-27 21:44:07 +0200

    Add SIM7600G-H driver to install script

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Install packages
-PACKAGES="python3-picamera2 minicom"
+PACKAGES="python3-picamera2 minicom p7zip-full"
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y $PACKAGES 
@@ -13,16 +13,21 @@ exec wget -O /home/pi/main.py https://raw.githubusercontent.com/Eagleshot/Glacie
 sudo chmod +x /home/pi/main.py # Execution permissions
 sudo sed -i '$i/home/pi/main.py &' /etc/rc.local
 
-# TODO Give python script permissions needed to run
+# Waveshare SIM7600G-H 4G/LTE HAT
+sudo raspi-config nonint do_serial 2 # Enable serial port communication
+wget https://www.waveshare.com/w/upload/2/29/SIM7600X-4G-HAT-Demo.7z
+7z x SIM7600X-4G-HAT-Demo.7z -r -o/home/pi
+sudo chmod 777 -R /home/pi/SIM7600X-4G-HAT-Demo
+sudo sed -i '$i sh /home/pi/SIM7600X-4G-HAT-Demo/Raspberry/c/sim7600_4G_hat_init &' /etc/rc.local
+cd /home/pi/SIM7600X-4G-HAT-Demo/Raspberry/c/bcm2835
+chmod +x configure && ./configure && sudo make && sudo make install
 
 # Enable camera and other hardware interfaces
 sudo raspi-config nonint do_camera 0
 sudo raspi-config nonint do_i2c 0
 
 # Disable LED and other unused hardware
-sudo raspi-config nonint do_led 1
-sudo raspi-config nonint do_spi 1
-sudo raspi-config nonint do_serial 1
+# sudo raspi-config nonint do_led 1
 
 # TODO Enable read-only filesystem
 
