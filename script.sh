90c059e412ead4731a36ff3182475ae6d160f872
Noel <75380532+Eagleshot@users.noreply.github.com>
2023-03-27 14:52:16 +0200

    Add sudo to install script

--- script.sh
+++ script.sh
@@ -4,9 +4,9 @@ echo "Beginning install script:"
 
 # Install packages
 PACKAGES="python3-picamera2"
-apt-get update
-agt-get upgrade -y
-apt-get install -y $PACKAGES 
+sudo apt-get update
+sudo agt-get upgrade -y
+sudo apt-get install -y $PACKAGES 
 
 # TODO Enable camera and other hardware interfaces
 # TODO Enable read-only filesystem
