760e4d1e7310ca545e95ebc8ab8db8e5b06146b6
Noel <75380532+Eagleshot@users.noreply.github.com>
2023-04-13 11:59:26 +0200

    Fix indentation error

--- main.py
+++ main.py
@@ -17,17 +17,17 @@ import io
 ###########################
 # TODO Automatic station name if name is already in use (maybe with lock file/hardware id?)
 cameraName = "station1"
-imgFileName =  datetime.today().strftime('%d%m%Y_%H%M_') + cameraName + ".jpg"
-imgFilePath = "/home/pi/" # Path where image is saved
+imgFileName = datetime.today().strftime('%d%m%Y_%H%M_') + cameraName + ".jpg"
+imgFilePath = "/home/pi/"  # Path where image is saved
 
 ###########################
 # Readings
 ###########################
 csvFileName = "diagnostics.csv"
 currentTime = datetime.today().strftime('%d-%m-%Y %H:%M')
-currentBatteryLevel = "80" # TODO
-currentTemperature = "25.5" # TODO
-currentSignalQuality = "" # TODO
+currentBatteryLevel = "80"  # TODO
+currentTemperature = "25.5"  # TODO
+currentSignalQuality = ""  # TODO
 currentGPSPosLat = ""
 currentGPSPosLong = ""
 error = ""
@@ -38,95 +38,104 @@ error = ""
 # See Waveshare documentation
 try:
     import serial
-	ser = serial.Serial('/dev/ttyUSB2',115200) # USB connection
-	ser.flushInput()
+    ser = serial.Serial('/dev/ttyUSB2', 115200)  # USB connection
+    ser.flushInput()
 except:
-	error += "Could not open serial port."
-	print("Could not open serial port.")
+    error += "Could not open serial port."
+    print("Could not open serial port.")
 
 power_key = 6
 rec_buff = ''
 rec_buff2 = ''
 time_count = 0
 
-def send_at2(command,back,timeout):
-	rec_buff = ''
-	ser.write((command+'\r\n').encode())
-	time.sleep(timeout)
-	if ser.inWaiting():
-		time.sleep(0.01 )
-		rec_buff = ser.read(ser.inWaiting())
-	if back not in rec_buff.decode():
-		print(command + ' ERROR')
-		print(command + ' back:\t' + rec_buff.decode())
-		return 0
-	else:
-		return rec_buff.decode()
+
+def send_at2(command, back, timeout):
+    rec_buff = ''
+    ser.write((command+'\r\n').encode())
+    time.sleep(timeout)
+    if ser.inWaiting():
+        time.sleep(0.01)
+        rec_buff = ser.read(ser.inWaiting())
+    if back not in rec_buff.decode():
+        print(command + ' ERROR')
+        print(command + ' back:\t' + rec_buff.decode())
+        return 0
+    else:
+        return rec_buff.decode()
 
 # Get GPS Position
-def send_at(command,back,timeout):
-	rec_buff = ''
-	ser.write((command+'\r\n').encode())
-	time.sleep(timeout)
-	if ser.inWaiting():
-		time.sleep(0.01 )
-		rec_buff = ser.read(ser.inWaiting())
-	if rec_buff != '':
-		if back not in rec_buff.decode():
-			print(command + ' ERROR')
-			print(command + ' back:\t' + rec_buff.decode())
-			return 0
-		elif ',,,,,,' in rec_buff.decode():
-			print('GPS is not ready')
-			return 0
-		else:
-			#Additions to Demo Code Written by Tim! -> Core Electronics
-			GPSDATA = str(rec_buff.decode())
-			Cleaned = GPSDATA[13:]
-			#print(Cleaned)
-			
-			Lat = Cleaned[:2]
-			SmallLat = Cleaned[2:11]
-			NorthOrSouth = Cleaned[12]
-			#print(Lat, SmallLat, NorthOrSouth)
-			
-			Long = Cleaned[14:17]
-			SmallLong = Cleaned[17:26]
-			EastOrWest = Cleaned[27]
-			#print(Long, SmallLong, EastOrWest)
-			
-			FinalLat = float(Lat) + (float(SmallLat)/60)
-			FinalLong = float(Long) + (float(SmallLong)/60)
-			
-			if NorthOrSouth == 'S': FinalLat = -FinalLat
-			if EastOrWest == 'W': FinalLong = -FinalLong
-			
-			FinalLongText = round(FinalLong, 7)
-			FinalLatText = round(FinalLat, 7)
-			
-			global currentGPSPosLat
-			global currentGPSPosLong
-			currentGPSPosLat = str(FinalLatText)
-			currentGPSPosLong = str(FinalLongText)
-			
-			print('Longitude:' + currentGPSPosLong + ' Degrees - Latitude: ' + currentGPSPosLat + ' Degrees')
-			
-			return 1
-	else:
-		print('GPS is not ready')
-		return 0
+
+
+def send_at(command, back, timeout):
+    rec_buff = ''
+    ser.write((command+'\r\n').encode())
+    time.sleep(timeout)
+    if ser.inWaiting():
+        time.sleep(0.01)
+        rec_buff = ser.read(ser.inWaiting())
+    if rec_buff != '':
+        if back not in rec_buff.decode():
+            print(command + ' ERROR')
+            print(command + ' back:\t' + rec_buff.decode())
+            return 0
+        elif ',,,,,,' in rec_buff.decode():
+            print('GPS is not ready')
+            return 0
+        else:
+            # Additions to Demo Code Written by Tim! -> Core Electronics
+            GPSDATA = str(rec_buff.decode())
+            Cleaned = GPSDATA[13:]
+            # print(Cleaned)
+
+            Lat = Cleaned[:2]
+            SmallLat = Cleaned[2:11]
+            NorthOrSouth = Cleaned[12]
+            #print(Lat, SmallLat, NorthOrSouth)
+
+            Long = Cleaned[14:17]
+            SmallLong = Cleaned[17:26]
+            EastOrWest = Cleaned[27]
+            #print(Long, SmallLong, EastOrWest)
+
+            FinalLat = float(Lat) + (float(SmallLat)/60)
+            FinalLong = float(Long) + (float(SmallLong)/60)
+
+            if NorthOrSouth == 'S':
+                FinalLat = -FinalLat
+            if EastOrWest == 'W':
+                FinalLong = -FinalLong
+
+            FinalLongText = round(FinalLong, 7)
+            FinalLatText = round(FinalLat, 7)
+
+            global currentGPSPosLat
+            global currentGPSPosLong
+            currentGPSPosLat = str(FinalLatText)
+            currentGPSPosLong = str(FinalLongText)
+
+            print('Longitude:' + currentGPSPosLong +
+                  ' Degrees - Latitude: ' + currentGPSPosLat + ' Degrees')
+
+            return 1
+    else:
+        print('GPS is not ready')
+        return 0
+
 
 ###########################
 # Setup camera
 ###########################
 camera = Picamera2()
-cameraConfig = camera.create_still_configuration({"size":(4608, 2592)}) # TODO
+cameraConfig = camera.create_still_configuration(
+    {"size": (4608, 2592)})  # TODO
 
 ###########################
 # Capture image
 ###########################
 try:
-    camera.start_and_capture_file(imgFilePath + imgFileName, capture_mode = cameraConfig, delay = 3, show_preview = False)
+    camera.start_and_capture_file(
+        imgFilePath + imgFileName, capture_mode=cameraConfig, delay=3, show_preview=False)
 except:
     error += "Could not start camera and capture image."
     print("Could not start camera and capture image.")
@@ -135,7 +144,7 @@ except:
 # Stop camera
 ###########################
 try:
-    camera.stop() 
+    camera.stop()
 except:
     error += "Camera already stopped."
     print("Camera already stopped.")
@@ -147,11 +156,12 @@ ftpServerAddress = config.ftpServerAddress
 username = config.username
 password = config.password
 
-ftp = FTP(ftpServerAddress, timeout = 180) # TODO consider FTP_TLS, timeout = 3 minutes
-ftp.login(user = username, passwd = password)
+# TODO consider FTP_TLS, timeout = 3 minutes
+ftp = FTP(ftpServerAddress, timeout=180)
+ftp.login(user=username, passwd=password)
 
 # ftp.dir() # Directory listing
-ftp.cwd("private") # Go to folder "private"
+ftp.cwd("private")  # Go to folder "private"
 
 ###########################
 # Upload last image
@@ -162,14 +172,14 @@ try:
 except:
     error += "Could not open image."
     print("Could not open image.")
-    
+
 ###########################
 # Upload main.py (this file)
 ###########################
 # TODO remove after development
 with open("/home/pi/main.py", 'rb') as file:
     ftp.storbinary(f"STOR {'main.py'}", file)
-    
+
 ###########################
 # Uploading sensor data to CSV
 ###########################
@@ -180,23 +190,23 @@ try:
     answer = 0
     print('Start GPS session.')
     rec_buff = ''
-    send_at('AT+CGPS=1,1','OK',1)
+    send_at('AT+CGPS=1,1', 'OK', 1)
     time.sleep(2)
     maxAttempts = 0
 
     while (maxAttempts <= 35):
         maxAttempts += 1
         answer = send_at('AT+CGPSINFO', '+CGPSINFO: ', 1)
-        if answer == 1: # Success
-            break    
+        if answer == 1:  # Success
+            break
         else:
-            print('error %d'%answer)
-            send_at('AT+CGPS=0','OK',1)
+            print('error %d' % answer)
+            send_at('AT+CGPS=0', 'OK', 1)
             time.sleep(1.5)
 except:
     error += "Failed to get GPS coordinates"
     print("Failed to get GPS coordinates")
-    
+
 # Get cell signal quality
 try:
     currentSignalQuality = send_at2('AT+CSQ', 'OK', 1)[8:13]
@@ -205,7 +215,8 @@ except:
     error += "Failed to get cell signal quality"
     print("Failed to get cell signal quality")
 # Upload data to server
-newRow = [currentTime, currentBatteryLevel, currentTemperature, currentSignalQuality, currentGPSPosLat, currentGPSPosLong, error]
+newRow = [currentTime, currentBatteryLevel, currentTemperature,
+          currentSignalQuality, currentGPSPosLat, currentGPSPosLong, error]
 
 # Append new measurements to log CSV or create new CSV file if none exists
 with io.StringIO() as csvBuffer:
@@ -218,15 +229,15 @@ with io.StringIO() as csvBuffer:
 # Download and read config file
 ###########################
 try:
-    with open('/home/pi/config.txt', 'wb') as fp: # Download
+    with open('/home/pi/config.txt', 'wb') as fp:  # Download
         ftp.retrbinary('RETR config.txt', fp.write)
 except:
     print("Could not find config file!")
-        
+
 ftp.quit()
 
-with open('/home/pi/config.txt', 'r') as fp: # Read
-    
-    if 'shutdown = true' in fp.read(): # Shutdown computer if defined in loop
+with open('/home/pi/config.txt', 'r') as fp:  # Read
+
+    if 'shutdown = true' in fp.read():  # Shutdown computer if defined in loop
         print('shutting down')
         os.system("sudo shutdown -h now")
