88cdf0a1729f5fcc42792b8d669a327327855d54
Noel <75380532+Eagleshot@users.noreply.github.com>
2023-05-29 18:15:07 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,4 @@
-lensPosition = 0.0 # min = 0.0, max = 32.0, default = 1.0
-recordingInterval = 20 # min
-startRecording = 5
-stopRecording = 22
+lensPosition = 0.0 # min = 0.0, max = 32.0, default = 1.0, autofocus = -1.0
 enableGPS = False # Log GPS coordinates
 shutdown = True # Enable/disable shutdown of pi after program has run
+# TODO Upload WittyPi timing script
