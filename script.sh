715c111a602884647af7c9e6058b98af796652f9
Noel <75380532+Eagleshot@users.noreply.github.com>
2023-06-19 19:15:49 +0200

    Updated installation script

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Check if sudo is used
 if [ "$(id -u)" != 0 ]; then
@@ -11,6 +11,8 @@ echo '==========================================================================
 echo '|                                                                              |'
 echo '|                   Glacier Camera Software Installation Script                |'
 echo '|                                                                              |'
+echo '|       Step 1: Update the Raspberry Pi and install required packages          |'
+echo '|                                                                              |'
 echo '================================================================================'
 echo ''
 
@@ -18,46 +20,78 @@ echo ''
 PACKAGES="minicom p7zip-full pyserial ufw" # picamera2 is preinstalled
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y $PACKAGES
+sudo apt-get install -y "$PACKAGES"
 sudo apt-get autoremove -y
 
-# TODO: Sudo ufw enable?
-# The firewall automatically blocks all incoming connections!
-# https://www.raspberrypi.com/documentation/computers/configuration.html#install-a-firewall
+echo ''
+echo '================================================================================'
+echo '|                                                                              |'
+echo '|       Step 2: Install WittyPi Software                                       |'
+echo '|                                                                              |'
+echo '================================================================================'
+# Install WittyPi Software 
+wget -O - https://www.uugear.com/repo/WittyPi4/install.sh | sudo sh
 
-# TODO Install wittyPi software
-exec wget https://www.uugear.com/repo/WittyPi4/install.sh && sudo sh install.sh
+echo ''
+echo '================================================================================'
+echo '|                                                                              |'
+echo '|       Step 3: Install Waveshare SIM7600G-H 4G/LTE HAT driver                  |'
+echo '|                                                                              |'
+echo '================================================================================'
+# Install Waveshare SIM7600G-H 4G/LTE HAT driver
+sudo apt-get install -y bcm2835
+sudo raspi-config nonint do_serial 2 # Enable serial port communication
 
-# # Waveshare SIM7600G-H 4G/LTE HAT
-# sudo raspi-config nonint do_serial 2 # Enable serial port communication
-# exec wget "https://www.waveshare.com/w/upload/4/4e/SIM7600X-4G-HAT(B)-Demo.7z" # Download SIm-7600G-H code
-# 7z x SIM7600X-4G-HAT-Demo.7z -r -o/home/pi/ # Unzip code
-# sudo chmod 777 -R /home/pi/SIM7600X-4G-HAT-Demo # Make code executable
-# sudo sed -i -e '$i sh /home/pi/SIM7600X-4G-HAT-Demo/Raspberry/c/sim7600_4G_hat_init &' /etc/rc.local
-# cd /home/pi/SIM7600X-4G-HAT-Demo/Raspberry/c/bcm2835
-# chmod +x configure && ./configure && sudo make && sudo make install
+wget "https://www.waveshare.com/w/upload/4/4e/SIM7600X-4G-HAT(B)-Demo.7z" -P /tmp # Download SIm-7600G-H code
+7z x /tmp/SIM7600X-4G-HAT\(B\)-Demo.7z -o/home/pi/ # Unzip code
+sudo chmod 777 -R /home/pi/SIM7600X-4G-HAT\(B\)-Demo # Make code executable
+sudo sed -i '$i sh /home/pi/SIM7600X-4G-HAT\(B\)-Demo/Raspberry/c/sim7600_4G_hat_init &' /etc/rc.local
+cd /home/pi/SIM7600X-4G-HAT\(B\)-Demo/Raspberry/c/bcm2835
+chmod +x configure && ./configure && make && sudo make install
 
-# # Download python script to /home/pi
-exec wget -O /home/pi/main.py https://raw.githubusercontent.com/Eagleshot/GlacierCam/main/main.py
+echo ''
+echo '================================================================================'
+echo '|                                                                              |'
+echo '|       Step 4: Install Python script                                          |'
+echo '|                                                                              |'
+echo '================================================================================'
+# Download python script to /home/pi
+wget -O /home/pi/main.py https://raw.githubusercontent.com/Eagleshot/GlacierCam/main/main.py
 sudo chmod +x /home/pi/main.py # Execution permissions
-# Enable python script to run on boot
-# TODO Do this with systemd
-# https://www.youtube.com/watch?v=DUGZC-tNm2w
-# sudo systemctl status myscript.service
 
 # Download config.py
-exec wget -O /home/pi/config.py https://raw.githubusercontent.com/Eagleshot/GlacierCam/main/config.py
+wget -O /home/pi/config.py https://raw.githubusercontent.com/Eagleshot/GlacierCam/main/config.py
 
 # Download settings.py
-exec wget -O /home/pi/settings.py https://raw.githubusercontent.com/Eagleshot/GlacierCam/main/settings.py
+wget -O /home/pi/settings.py https://raw.githubusercontent.com/Eagleshot/GlacierCam/main/settings.py
 
-# Add python "/home/pi/main.py" wittyPi afterStartup.sh
+# Add main.py to wittyPi afterStartup.sh
+echo "/usr/bin/python3 /home/pi/main.py" >> /home/pi/wittyPi/afterStartup.sh
+
+echo ''
+echo '================================================================================'
+echo '|                                                                              |'
+echo '|       Step 5: Configure Raspberry Pi                                         |'
+echo '|                                                                              |'
+echo '================================================================================'
+echo ''
+
 # Enable camera and other hardware interfaces
 sudo raspi-config nonint do_camera 0
 
 # Disable LED and other unused hardware
-# sudo raspi-config nonint do_led 1
+echo "boot_delay=0" | sudo tee -a /boot/config.txt
+echo "disable_splash=1" | sudo tee -a /boot/config.txt
+echo "dtparam=act_led_trigger=none" | sudo tee -a /boot/config.txt
+echo "dtoverlay=disable-bt" | sudo tee -a /boot/config.txt
+echo "dtoverlay=disable-wifi" | sudo tee -a /boot/config.txt
 
-# TODO Enable read-only filesystem
+# Enable the firewall
+sudo ufw enable
 
-
+echo ''
+echo '================================================================================'
+echo '|                                                                              |'
+echo '|              Glacier Camera Software Installation Completed!  :)             |'
+echo '|                                                                              |'
+echo '================================================================================'
