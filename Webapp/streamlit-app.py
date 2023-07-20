09111028869321ebf4bedb4501c971ff09ba1265
Noel <75380532+Eagleshot@users.noreply.github.com>
2023-07-21 00:11:15 +0200

    WIP Webapp

--- Webapp/streamlit-app.py
+++ Webapp/streamlit-app.py
@@ -3,35 +3,28 @@ from ftplib import FTP
 from io import BytesIO
 from PIL import Image
 import pandas as pd
-import yaml
+from yaml import safe_load
 from datetime import datetime
+import matplotlib.pyplot as plt
 
 # FTP server credentials
 FTP_HOST = '444024-1.web.fhgr.ch'
 FTP_USERNAME = '444024_1_1'
 FTP_PASSWORD = 'e5tlZOhT=EoB'
+FTP_FOLDER = 'private/CameraX_00000000baed1475'
 
 # Connect to the FTP server
 ftp = FTP(FTP_HOST)
 ftp.login(user=FTP_USERNAME, passwd=FTP_PASSWORD)
 
-ftp.cwd('private')
-
+# Change the working directory to the FTP folder
+ftp.cwd(FTP_FOLDER)
 
 # Streamlit app
 def main():
 
     # Title
     st.title("Camera")
-
-    # Make a selection for all folders in the FTP server
-    folders = ftp.nlst()
-
-    # Select a folder from the FTP server
-    selected_folder = st.selectbox("Select a folder", folders)
-
-    # Change the working directory to the selected folder
-    ftp.cwd(selected_folder)
 
     # Download diagnosticsf.csv as file with utf-8 encoding
     ftp.retrbinary('RETR diagnostics.csv', open('df.csv', 'wb').write)
@@ -50,7 +43,7 @@ def main():
     df.rename(columns={df.columns[9]: 'Error'}, inplace=True)
 
     # Drop the last row
-    df.drop(df.tail(1).index, inplace=True)
+    # df.drop(df.tail(1).index, inplace=True)
 
     # Format of timestamp: 27062023_1912
     df["Day"] = df["Timestamp"].str[:2]
@@ -70,13 +63,23 @@ def main():
     # Only show the image files
     files = [file for file in files if file.endswith(".jpg")]
 
-    # Select an image file from the FTP server
-    selected_file = st.selectbox("Select an image file", files)
+    # Get number of files
+    number_of_files = len(files)
 
-    # oldestDate = df["Timestamp"].min()
-    # print(oldestDate)
-
-    # # https://docs.kanaries.net/de/tutorials/Python/streamlit-datetime-slider.de
+    # Slider with images
+    selected_file = st.slider(
+        "Select an image",
+        min_value=0,
+        max_value=number_of_files-1,
+        value=number_of_files-1,
+        step=1,
+        label_visibility="collapsed"
+    )
+    
+    selected_file = files[selected_file]
+    
+    # TODO: Maybe use this slider for the time
+    # https://docs.kanaries.net/de/tutorials/Python/streamlit-datetime-slider.de
     # start_time = st.slider(
     #     "When do you start?",
     #     min_value=datetime(2020, 1, 1, 9, 0),
@@ -93,8 +96,36 @@ def main():
     # Rotate the image
     image = image.rotate(180, expand=True)
 
-    # Display the image
-    st.image(image, caption=selected_file, use_column_width=True)
+    # Display the image with the corresponding timestamp
+    st.image(image, use_column_width=True)
+
+    # Download button for image
+    st.download_button(
+        label="Bild herunterladen 📷",
+        data=image_data,
+        file_name=selected_file,
+        mime="image/jpeg",
+        use_container_width=True
+    )
+
+    # Print last Battery Voltage
+    st.write("Batteriespannung: ", df['Battery Voltage'].iloc[-1])
+
+    # Print next startup relative to now
+    nextStartup = df['Next Startup'].iloc[-1]
+    nextStartup = datetime.strptime(nextStartup, '%Y-%m-%d %H:%M:%S')
+    now = datetime.now()
+    timeDifference = nextStartup - now
+    # Write difference in hours and minutes
+    nextStartText = "Nächster Start in " 
+    if timeDifference.seconds//3600 > 0:
+        nextStartText = nextStartText + str(timeDifference.seconds//3600) + " Stunden und " + str((timeDifference.seconds//60)%60) + " Minuten."
+    elif (timeDifference.seconds//60)%60 > 1:
+        nextStartText = nextStartText + str((timeDifference.seconds//60)%60) + " Minuten."
+    else:
+        nextStartText = nextStartText + "weniger als eine Minute."
+    st.write(nextStartText)
+    # TODO letzte Bildaufnahme
 
     # # Plot the battery voltage
     # #st.line_chart(df['Battery Voltage'])
@@ -112,24 +143,39 @@ def main():
     st.dataframe(df)
 
     # Show a map with the location of the camera
-    last_latitude = float(df['Latitude'].iloc[-1])
-    last_longitude = float(df['Longitude'].iloc[-1])
-    st.map(pd.DataFrame({'lat': [last_latitude], 'lon': [last_longitude]}))
+    # Get the last latitude and longitude where it is not "-"
+    df = df[df['Latitude'] != '-']
+    df = df[df['Longitude'] != '-']
+
+    try: 
+        # Convert the latitude and longitude to float
+        last_latitude = float(df['Latitude'].iloc[-1])
+        last_longitude = float(df['Longitude'].iloc[-1])
+        st.map(pd.DataFrame({'lat': [last_latitude], 'lon': [last_longitude]}))
+
+        # Print timestamp
+        st.write("Letztes Update: ", df['Timestamp'].iloc[-1])
+    except:
+        st.write("Keine Koordinaten vorhanden")
 
     # Read settings.yaml and display it
     ftp.retrbinary('RETR settings.yaml', open('settings.yaml', 'wb').write)
     
     with open('settings.yaml') as file:
-        dfSettings = yaml.load(file, Loader=yaml.FullLoader)
+        dfSettings = safe_load(file)
 
     # Display the settings
     st.dataframe(dfSettings)
 
 
-
-
-    # Close the FTP connection
-    # ftp.quit()
+    # Download diagnostics.csv
+    st.download_button(
+        label="Diagnosedatei herunterladen 📝",
+        data=df.to_csv().encode("utf-8"),
+        file_name="diagnostics.csv",
+        mime="text/csv",
+        use_container_width=True
+    )
     
 
 # Run the app
