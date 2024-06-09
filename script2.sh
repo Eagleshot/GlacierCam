a728708d4bce4cf69cd8503fa369ab07ba1fb78f
eagleshot <****@****.com>
2024-06-09 12:50:34 +0200

    Short sleep before update

--- script2.sh
+++ script2.sh
@@ -4,6 +4,7 @@ echo 'Configuring software to run on startup...'
 echo "sudo /usr/bin/python3 /home/pi/main.py" >> /home/pi/wittypi/afterStartup.sh
 
 # Software update possibility
+echo "sleep 5" >> /home/pi/wittypi/afterStartup.sh
 echo "sudo wget -O - https://raw.githubusercontent.com/Eagleshot/GlacierCam/main/updateScript.sh | sudo sh" >> /home/pi/wittypi/afterStartup.sh
 
 # Schedule shutdown after update
