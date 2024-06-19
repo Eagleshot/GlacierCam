54ce80a3be8403580d5a3a29775cb1324afdede5
eagleshot <****@****.com>
2024-06-19 21:45:55 +0200

    Update wakeup watchdog time to 2 days

--- script3.sh
+++ script3.sh
@@ -2,12 +2,12 @@
 
 # Configure the wake-up watchdog
 echo 'Configuring the wake-up watchdog (guaranteed wake mode)...'
-i2cset -y 0x01 0x08 49 0x81
+i2cset -y 0x01 0x08 49 0x82
 
 # Check the config
 echo 'Checking the wake-up watchdog configuration...'
 i2cget -y 0x01 0x08 49
-echo 'Should return 0x81. If not, please check the configuration.'
+echo 'Should return 0x82. If not, please check the configuration.'
 
 # Check the RTC offset
 echo 'Checking the RTC offset...'
